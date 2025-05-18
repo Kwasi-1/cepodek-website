19f459e981f7a4374e22b8b9f46c4d19214b4990
kwasi <affulnana04@gmail.com>
2025-05-18 07:14:36 +0000

    chnage footor links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
 import logo from "@/assets/images/logo.png";
 import { Link } from "react-router-dom";
+import { Mail, MapPin, Phone } from "lucide-react";
 
 const quickLinks = [
   { label: "Home", href: "/" },
@@ -22,29 +23,29 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto py-16 lg:py-24 border-b border-white/50 px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {/* Logo */}
         <div className="col-span-1">
-          <img src={logo} alt="Logo" className="h-12 sm:h-16 mb-4" />
-        </div>
+          <img src={logo} alt="Logo" className="h-12 sm:h-14 mb-4" />
+          <p className="text-base text-[#F5F5F5] leading-[150%] font-[300] mb-4">
+            Your trusted FMCG partner in Ghana, providing retail, wholesale, and
+            delivery services nationwide.
+          </p>
 
-        {/* Contact Info */}
-        <div className="flex flex-col gap-4 text-[15px] col-span-1  sm:text-base lg:text-lg leading-[150%] font-[300]">
           <div>
-            <p className="sm:text-base lg:text-lg flex items-center gap-1 mb-4">
-              <Icon icon={`famicons:location-outline`} className="h-6 w-6" />
-              Location 1: Oyarifa
-            </p>
-            <p className="sm:text-base lg:text-lg flex items-center gap-1">
-              <Icon icon={`famicons:location-outline`} className="h-6 w-6" />
-              Location 2: Madina
-            </p>
-          </div>
-          <div className="sm:text-base lg:text-lg flex items-center gap-1">
-            <Icon icon={`mdi-light:phone`} className="h-6 w-6" />
-            +233 54 012 8958{" "}
+            <div className="flex space-x-4">
+              {socialLinks.map(({ icon, href }, idx) => (
+                <a
+                  key={idx}
+                  href={href}
+                  className="hover:text-primary/80 p-3 border border-white/30 rounded-full transition-colors duration-200"
+                >
+                  <Icon icon={icon} className="h-4 w-4" />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
 
         {/* Quick Links */}
-        <div>
+        <div className="lg:ml-10">
           <h4 className="text-white mb-2 text-lg lg:text-xl leading-[150%] font-[600]">
             Quick Links
           </h4>
@@ -53,7 +54,7 @@ export default function Footer() {
               <li key={label}>
                 <Link
                   to={href}
-                  className="hover:text-primary/80 transition-colors duration-200 text-base sm:text-lg leading-[150%] font-[300]"
+                  className="transition-colors duration-200 text-base leading-[150%] opacity-90 hover:text-[#FFC20E] "
                 >
                   {label}
                 </Link>
@@ -62,22 +63,48 @@ export default function Footer() {
           </ul>
         </div>
 
-        {/* Social Icons */}
         <div>
-          <h4 className="text-white mb-2  text-lg lg:text-xl leading-[150%] font-[600]">
-            Follow Us
-          </h4>
-          <div className="flex space-x-4">
-            {socialLinks.map(({ icon, href }, idx) => (
-              <a
-                key={idx}
-                href={href}
-                className="hover:text-primary/80 p-3 border border-white/30 rounded-full transition-colors duration-200"
-              >
-                <Icon icon={icon} className="h-4 w-4" />
+          <h4 className="text-lg font-semibold mb-4">Help & Info</h4>
+          <ul className="space-y-2 opacity-80">
+            <li>
+              <a href="#" className="hover:text-[#FFC20E] transition-colors">
+                Delivery Info
               </a>
-            ))}
-          </div>
+            </li>
+            <li>
+              <a href="#" className="hover:text-[#FFC20E] transition-colors">
+                Wholesale Accounts
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-[#FFC20E] transition-colors">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-[#FFC20E] transition-colors">
+                Terms & Conditions
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4 className="text-lg font-semibold mb-4">Contact Us</h4>
+          <ul className="space-y-3 opacity-80">
+            <li className="flex items-start">
+              <Phone className="h-5 w-5 mr-2 mt-0.5" />
+              <span> +233 54 012 8958</span>
+            </li>
+            <li className="flex items-start">
+              <Mail className="h-5 w-5 mr-2 mt-0.5" />
+              <span>info@cepodek.com</span>
+            </li>
+            <li className="flex items-start">
+              <MapPin className="h-5 w-5 mr-2 mt-0.5" />
+              <span>Madina Market, Ghana</span>
+            </li>
+          </ul>
         </div>
       </div>
 
